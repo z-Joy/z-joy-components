7bdda4890169a00973cb1831aef0229b3491eac6
zhengjinhan <jinhan.zheng@inmeijia.com>
2020-06-02 11:47:16 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h2>头像组件</h2>
+    <avatar></avatar>
+  </div>
+</template>
+
+<script>
+import avatar from '@/components/avatar/index'; // 引入
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  },
+  components:{ //注册插件
+    avatar
+  },
+  methods:{
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/avatar/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="avatar">
+    <img :src="src" alt="头像" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'avatar',
+  props:{
+    src: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.avatar {
+  width: 40px;
+  height: 40px;
+  border-radius: 100%;
+  background-color: #ccc;
+}
+</style>
